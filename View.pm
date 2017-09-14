3a4e61fb4fa1e4a48dca0772e53abe32e7a4d1e4
Alexander McClelland <agmcclelland@yahoo.com>
2017-09-14 21:09:04 +0100

    Create View class

---
+++ View.pm
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+
+package Oyster::View;
+
+use strict;
+use warnings;
+
+# Constructor
+
+sub new {
+	my ($class) = @_;
+	my $self = {
+
+	};
+	bless ($self, $class);
+	return $self;
+}
+
+# Instance methods
+
+1;
